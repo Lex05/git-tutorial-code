b73e995179d3cb6f52bd810da0b5ae0bbc340ffe
Lex05 <lea.matlekovic@gmail.com>
2017-04-06 18:47:33 +0200

    2

--- python/lust.py
+++ python/lust.py
@@ -25,7 +25,7 @@ class FactorialCommand(LustObject):
 
   def __calculate_factorial(self, argument):
     # Hmmm...
-	i = 0
+	
     for i in range(1, argument+1):
       result *= i
     return result
